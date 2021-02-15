d8a49fed6a1f0d9800a3d592e2031c498e9317fd
olovm <olov.mckie@ub.uu.se>
2021-02-15 16:06:31 +0000

    removing unused imports

--- src/test/java/se/uu/ub/cora/converter/ConverterInitializationExceptionTest.java
+++ src/test/java/se/uu/ub/cora/converter/ConverterInitializationExceptionTest.java
@@ -22,8 +22,6 @@ import static org.testng.Assert.assertEquals;
 
 import org.testng.annotations.Test;
 
-import se.uu.ub.cora.converter.ConverterInitializationException;
-
 public class ConverterInitializationExceptionTest {
 	@Test
 	public void testInit() {
